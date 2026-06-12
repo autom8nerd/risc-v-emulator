7ceb18a2b47b7ffeb290b4d572d72f696370dbdd
iamancil <24bcs015@nith.ac.in>
2026-06-12 17:56:37 +0530

    gotta find out the funct3 and funct7

--- include/decoder.h
+++ include/decoder.h
@@ -4,5 +4,13 @@
 
 #ifndef RISC_V_EMULATOR_DECODER_H
 #define RISC_V_EMULATOR_DECODER_H
-
+#include <stdint.h>
+typedef struct DecodedInstruction{
+    uint8_t opcode;
+    uint8_t rd;
+    uint8_t rs1;
+    uint8_t rs2;
+    uint8_t funct3;
+    uint8_t funct7;
+} DecodedInstruction;
 #endif //RISC_V_EMULATOR_DECODER_H
--- src/decoder.c
+++ src/decoder.c
@@ -1,3 +1,11 @@
-//
-// Created by fsoc on 12/06/26.
-//
+#include "../include/decoder.h"
+
+DecodedInstruction  *instr;
+
+void decode(const uint32_t instruction) {
+    instr->opcode = instruction & 0x7F;
+    instr->rd = (instruction >> 7) & 0x1F;
+    instr->rs1 = (instruction>>15) & 0x1F;
+    instr->rs2 = (instruction >> 20) & 0x1F;
+    instr->funct3 = (instruction>>12) & 0x7;
+}
