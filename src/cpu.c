474353ef9f9732c472ef5c8e3c50f0a63ff35115
iamancil <24bcs015@nith.ac.in>
2026-06-12 11:45:22 +0530

    initial commit

--- src/cpu.c
+++ src/cpu.c
@@ -2,12 +2,12 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "ram.c"
 
 uint32_t IALIGN = 32;
 uint32_t reg[32] = {0};
 uint32_t pc = 0x00000;
 
-#define RAM_SIZE (1024 * 1024)
 uint8_t memory[RAM_SIZE];
 
 /*
@@ -18,40 +18,6 @@ uint8_t memory[RAM_SIZE];
    with register x5 available as an alternate link register.
    The standard calling convention uses register x2 as the stack pointer.
  */
-enum {
-  x0 = 0,
-  x1, // return address register
-  x2, // stack pointer
-  x3,
-  x4,
-  x5,
-  x6,
-  x7,
-  x8,
-  x9,
-  x10,
-  x11,
-  x12,
-  x13,
-  x14,
-  x15,
-  x16,
-  x17,
-  x18,
-  x19,
-  x20,
-  x21,
-  x22,
-  x23,
-  x24,
-  x25,
-  x26,
-  x27,
-  x28,
-  x29,
-  x30,
-  x31
-};
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
