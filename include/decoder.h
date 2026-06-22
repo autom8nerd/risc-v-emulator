e8d5b05923cc2b0c2607116d13c5dbd2accc425c
iamancil <24bcs015@nith.ac.in>
2026-06-22 22:37:42 +0530

    added the enum for instructions

--- include/decoder.h
+++ include/decoder.h
@@ -12,5 +12,22 @@ typedef struct DecodedInstruction{
     uint8_t rs2;
     uint8_t funct3;
     uint8_t funct7;
+    uint8_t imm;
+    uint8_t type;
 } DecodedInstruction;
+DecodedInstruction decode(uint32_t);
+typedef enum {
+    INST_ADD,
+    INST_SUB,
+    INST_ADDI,
+    INST_LW,
+    INST_SW,
+    INST_BEQ,
+    INST_BNE,
+    INST_JAL,
+    INST_JALR,
+    INST_LUI,
+    INST_AUIPC,
+    INST_UNKNOWN
+} InstructionType;
 #endif //RISC_V_EMULATOR_DECODER_H
