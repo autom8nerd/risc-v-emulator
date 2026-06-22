942c484b982e3dea9b334854b34b4428bcb0ce47
iamancil <24bcs015@nith.ac.in>
2026-06-22 22:38:06 +0530

    ADD, ADDI and SUB only

--- src/decoder.c
+++ src/decoder.c
@@ -1,11 +1,32 @@
 #include "../include/decoder.h"
 
-DecodedInstruction  *instr;
 
-void decode(const uint32_t instruction) {
-    instr->opcode = instruction & 0x7F;
-    instr->rd = (instruction >> 7) & 0x1F;
-    instr->rs1 = (instruction>>15) & 0x1F;
-    instr->rs2 = (instruction >> 20) & 0x1F;
-    instr->funct3 = (instruction>>12) & 0x7;
+DecodedInstruction decode(const uint32_t instruction) {
+    DecodedInstruction instr;
+    instr.opcode = instruction & 0x7F;
+    instr.rd = (instruction >> 7) & 0x1F;
+    instr.rs1 = (instruction >> 15) & 0x1F;
+    instr.rs2 = (instruction >> 20) & 0x1F;
+    instr.funct3 = (instruction >> 12) & 0x7;
+    instr.funct7 = (instruction >> 25) & 0x7F;
+    instr.imm = INST_UNKNOWN;
+    if(instr.opcode == 0x33)
+    {
+        if(instr.funct3 == 0 &&
+           instr.funct7 == 0)
+        {
+            instr.type = INST_ADD;
+        }
+        else if(instr.funct3 == 0 &&
+                instr.funct7 == 0x20)
+        {
+            instr.type = INST_SUB;
+        }
+    }
+    if(instr.opcode == 0x13 &&
+   instr.funct3 == 0)
+    {
+        instr.type = INST_ADDI;
+    }
+    return instr;
 }
