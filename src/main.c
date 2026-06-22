2914bfa6a6f9060163272afa424d2908865e9eeb
iamancil <24bcs015@nith.ac.in>
2026-06-22 13:37:06 +0530

    test successful

--- src/main.c
+++ src/main.c
@@ -1,3 +1,13 @@
-//
-// Created by fsoc on 12/06/26.
-//
+#include "../include/memory.h"
+#include "../include/bus.h"
+#include <stdio.h>
+Memory memory;
+Bus bus;
+int main() {
+    init_memory(&memory);
+    init_bus(&bus, &memory);
+
+    bus_write(&bus, 0x100, 42);
+
+    printf("%u\n", bus_read(&bus, 0x100));
+}
