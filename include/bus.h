8cc8e858782d3e31f5761b00f2033a32e466341e
iamancil <24bcs015@nith.ac.in>
2026-06-22 13:36:28 +0530

    added basic functions

--- include/bus.h
+++ include/bus.h
@@ -4,5 +4,12 @@
 
 #ifndef RISC_V_EMULATOR_BUS_H
 #define RISC_V_EMULATOR_BUS_H
+#include "../include/memory.h"
+typedef struct {
+    Memory* memory;
+} Bus;
 
+void init_bus(Bus* bus, Memory* memory);
+void bus_write(const Bus *bus, uint32_t address, uint8_t value);
+uint8_t bus_read(const Bus *bus, uint32_t address);
 #endif //RISC_V_EMULATOR_BUS_H
--- src/bus.c
+++ src/bus.c
@@ -1,3 +1,11 @@
-//
-// Created by fsoc on 12/06/26.
-//
+#include "../include/bus.h"
+#include "../include/memory.h"
+void init_bus(Bus* bus, Memory* memory) {
+    bus->memory = memory;
+}
+void bus_write(const Bus *bus, const uint32_t address, const uint8_t value){
+    mem_write(bus->memory, address, value);
+}
+uint8_t bus_read(const Bus *bus, const uint32_t address) {
+    return mem_read(bus->memory, address);
+}
