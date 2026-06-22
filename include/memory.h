7c909c968b13d159ca992ae8477e000ed03982b5
iamancil <24bcs015@nith.ac.in>
2026-06-22 16:01:26 +0530

    added mem_read32

--- include/memory.h
+++ include/memory.h
@@ -10,6 +10,7 @@ typedef struct {
     uint8_t mem[MEMORY_SIZE];
 } Memory;
 void init_memory(Memory* memory);
-uint8_t mem_read(const Memory* memory, uint32_t address);
+uint8_t mem_read8(const Memory* memory, uint32_t address);
 void mem_write(Memory* memory, uint32_t address, uint8_t value);
+uint32_t mem_read32(const Memory* memory, uint32_t address);
 #endif //RISC_V_EMULATOR_MEMORY_H
--- src/memory.c
+++ src/memory.c
@@ -1,7 +1,7 @@
 #include "../include/memory.h"
 
 #include <stddef.h>
-
+#include "../include/cpu.h"
 void init_memory(Memory* memory) {
     for (size_t i = 0; i < MEMORY_SIZE; i++) {
         memory->mem[i] = 0;
@@ -11,6 +11,14 @@ void mem_write(Memory* memory, const uint32_t address, uint8_t value) {
     memory->mem[address] = value;
 }
 
-uint8_t mem_read(const Memory* memory, uint32_t address) {
+uint8_t mem_read8(const Memory* memory, uint32_t address) {
     return memory->mem[address];
+}
+uint32_t mem_read32(const Memory* memory, uint32_t address){
+    uint32_t result =
+        (uint32_t)memory->mem[address] |
+        ((uint32_t)memory->mem[address+1] << 8) |
+        ((uint32_t)memory->mem[address+2] << 16) |
+        ((uint32_t)memory->mem[address+3] << 24);
+    return result;
 }
