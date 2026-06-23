b08085d8496f739c2de1472e567efd6ab5bb4adb
iamancil <24bcs015@nith.ac.in>
2026-06-23 07:14:14 +0530

    added cpu_fetch and execute

--- include/cpu.h
+++ include/cpu.h
@@ -2,14 +2,15 @@
 // Created by fsoc on 12/06/26.
 //
 
-#ifndef RISC_V_EMULATOR_RAM_H
-#define RISC_V_EMULATOR_RAM_H
+#ifndef RISC_V_EMULATOR_CPU_H
+#define RISC_V_EMULATOR_CPU_H
+#include "../include/decoder.h"
 #include <stdint.h>
-#include  "../include/memory.h"
+#include "../include/bus.h"
 typedef struct {
     uint32_t reg[1<<5];
     uint32_t pc;
-    MEMORY* memory;
+    Bus* bus;
 } CPU;
 enum Register {
     x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24,
@@ -17,5 +18,7 @@ enum Register {
 };
 
 /*x1 holds the return instruction address, x2 is used as stack pointer and x5 as an alternative*/
-void init_cpu(CPU* cpu);
-#endif //RISC_V_EMULATOR_RAM_H
+void init_cpu(CPU* cpu, Bus* bus);
+uint32_t cpu_fetch32(const CPU* cpu);
+void execute(CPU* cpu, const DecodedInstruction* instr);
+#endif //RISC_V_EMULATOR_CPU_H
